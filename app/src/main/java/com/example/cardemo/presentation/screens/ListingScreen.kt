c880deb9cb90fe22189b3133c4a481467f23ac63
Dinesh Gadri <denygadri022@gmail.com>
2025-01-28 13:09:11 +0530

    removed unused code

--- app/src/main/java/com/example/cardemo/presentation/screens/ListingScreen.kt
+++ app/src/main/java/com/example/cardemo/presentation/screens/ListingScreen.kt
@@ -1,17 +1,12 @@
 package com.example.cardemo.presentation.screens
 
-import android.widget.Toast
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
