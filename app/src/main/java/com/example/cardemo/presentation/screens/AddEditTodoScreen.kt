e7299f4d141619b5a0f5cc4e8b2d60d27a6805ee
Dinesh Gadri <denygadri022@gmail.com>
2025-01-28 13:23:36 +0530

    Refactor the code

--- app/src/main/java/com/example/cardemo/presentation/screens/AddEditTodoScreen.kt
+++ app/src/main/java/com/example/cardemo/presentation/screens/AddEditTodoScreen.kt
@@ -3,16 +3,11 @@ package com.example.cardemo.presentation.screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.cardemo.core.common.UiEvent
 import com.example.cardemo.presentation.viewmodel.ProductDetailVewModel
 
